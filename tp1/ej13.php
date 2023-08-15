a8506a67b0c65453037f9677f50c26f936ecd1b8
facuhomola <66138918+facuhomola@users.noreply.github.com>
2023-08-15 17:32:36 +0200

    Add files via upload

---
+++ tp1/ej13.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../includes/css/style-index.css">
+    <link rel="stylesheet" type="text/css" href="../includes/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../includes/css/bootstrap.min.css">
+    <title>Ejercicio 13</title>
+</head>
+<body>
+    
+    <?php require '../includes/encabezado.html' ?>
+
+    <section>
+        <h3 style="text-align: center; padding: 15%;">Ejemplo de cuerpo de pagina</h3>
+    </section>
+
+    <?php require '../includes/pie.html' ?>
+
+    <script src="includes/js/bootstrap.min.js"></script>
+    <script src="includes/js/bootstrap.js"></script>
+</body>
+</html>
