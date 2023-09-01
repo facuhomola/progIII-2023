d03b60863ab0d2ada3cdd3872d64698c50946322
facuhomola <66138918+facuhomola@users.noreply.github.com>
2023-09-02 00:53:32 +0200

    Add files via upload

---
+++ tp2/ej-5/datos.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 5</title>
+</head>
+<body>
+    <h4>Formulario de registro</h4>
+    <form method="post" action="cobrar.php">
+        Nombre <input type="text" name="nombre"> <br>
+        
+    </form>
+</body>
+</html>
